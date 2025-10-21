cd53e98c2dedf011b5e89c2ae1ce0f105636e15d
solidity247 <solidity247@proton.me>
2025-10-20 20:49:39 -0400

    added primitive landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16" />
+          <header className="" />
           {children}
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="">HOME PAGE</div>;
+  return (
+    <div className="bg-muted flex min-h-svh items-center justify-center p-6 md:p-10">
+      <div className="z-2 bg-muted p-3 border-0 rounded-xl">
+        <h1 className="text-3xl">What are you waiting for?</h1>
+        <Button asChild>
+          <Link href="/portal">{"Let's play!"}</Link>
+        </Button>
+      </div>
+      <div className="w-full max-w-sm md:max-w-4xl z-1">
+        <Image
+          src="/images/landing-grayish.png"
+          alt="Landing page image"
+          fill
+          className="object-contain object-right min-h-dvh"
+          priority
+        />
+      </div>
+    </div>
+  );
 }
